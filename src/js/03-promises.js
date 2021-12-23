f26c7476472edb8957ce9a9a3c217ff53691108b
Mark-Nelovka <87484591+Mark-Nelovka@users.noreply.github.com>
2021-12-24 00:41:03 +0200

    Task 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,61 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const refs = {
+  formPromises: document.querySelector('.form'),
+  delayTime: document.querySelector('input[name=delay]'),
+  stepTime: document.querySelector('input[name=step]'),
+  amountSteps: document.querySelector('input[name=amount]'),
+  btnCreatePromises: document.querySelector('button')
+}
+
+refs.formPromises.addEventListener('submit', makePromises)
+
+let formValue = {};
+
+function makePromises(e) {
+  e.preventDefault();
+
+  formValue = {
+    delay: refs.delayTime.value,
+    step: refs.stepTime.value,
+    amount: refs.amountSteps.value
   }
+
+  let delayNumber = Number(formValue.delay);
+    let stepNumber = Number(formValue.step);
+  let amountNumber = Number(formValue.amount);
+ 
+  resultPrimises(delayNumber, stepNumber, amountNumber);
+
+}
+
+function createPrimises(position, delay) {
+ 
+  return new Promise((resolve, reject) => {
+      const shouldResolve = Math.random() > 0.3;
+       if (shouldResolve) {
+resolve(`✅ Fulfilled promise ${position} in ${delay} ms`)
+     } else {
+reject(`❌ Rejected promise ${position} in ${delay} ms`);
+     }
+   
+})
+}
+  
+function resultPrimises(delayNumber, stepNumber, amountNumber) {
+  let counter = 0;
+      for (let i = delayNumber; i < 10000; i += stepNumber) {
+        setTimeout(() => {
+   
+          counter += 1;
+          
+             if (counter > amountNumber) {
+              return;
+             }
+          
+          createPrimises(counter, i).then(result => Notiflix.Notify.success(`${result}`)).catch(result => Notiflix.Notify.warning(`${result}`));
+          return;
+        }, i);
+        
+      }
 }
